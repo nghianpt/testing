58beb9115452f959c4bb2b1bd780efff245dff14
NghiaNguyen <nghianpt@vng.com.vn>
2018-01-08 17:15:34 +0700

    first commit

---
+++ app/src/main/java/mylog/mto/vng/com/mylogutil/MainActivity.java
@@ -0,0 +1,15 @@
+package mylog.mto.vng.com.mylogutil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zing.zalo.zalosdk.oauth.ZaloSDK;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
